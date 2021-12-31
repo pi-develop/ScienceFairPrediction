6ae4ebc5bc6c284242fd6d37f1d57c5404182ce0
pi-develop <joshuazyli@gmail.com>
2021-12-30 18:07:57 -1000

    Update ui.r

--- ui.r
+++ ui.r
@@ -1,21 +1,11 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
 
-# Define UI for application that draws a histogram
 shinyUI(fluidPage(
     
     # Application title
     titlePanel("Covid SIR rates"),
     
-    # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
             sliderInput("vaccinated",
